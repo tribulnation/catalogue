dddcaf843b792b690e113fdc760d893d36bc7378
Marcel <marcel@tribulnation.com>
2026-06-22 16:53:48 +0000

    add exports

--- lib/src/tribulnation/catalogue/__init__.py
+++ lib/src/tribulnation/catalogue/__init__.py
@@ -2,4 +2,8 @@
 
 Central catalogue of (crypto-)assets, platforms and networks.
 """
-from .schema import Asset, BasePlatform, CexPlatform, DexPlatform, Blockchain, Platform, Catalogue, Spot, Perpetual
+from .schema import (
+  Asset, ExternalIds,
+  BasePlatform, CexPlatform, DexPlatform, Blockchain, Platform,
+  Catalogue, Spot, Perpetual,
+)
